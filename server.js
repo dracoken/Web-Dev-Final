77461de52bc0618b8fe190b00db26cdf8d8cf052
alexsnyder4 <129345536+alexsnyder4@users.noreply.github.com>
2023-04-21 19:29:51 -0400

    updated log in

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ app.use('/assests', express.static(path.join(__dirname, '/public')));
 app.get("/", (req,res) =>
 {
     //index = fetch()
-    res.sendFile(path.join(__dirname , 'public/htmlFiles/combatScene.html'));
-    //res.sendFile(path.join(__dirname, "public/htmlFiles/index.html"));
+    //res.sendFile(path.join(__dirname , 'public/htmlFiles/combatScene.html'));
+    res.sendFile(path.join(__dirname, "public/htmlFiles/index.html"));
 })
 
 app.post("/register",express.json(), async (req,res) => {
@@ -48,6 +48,7 @@ app.post("/register",express.json(), async (req,res) => {
             }
         })
         console.log(newAccount);
+        window.location.href = "public/htmlFiles/combatScene.html";
         return res.status(200).json({success:"signed up successfully"});
     }
     catch(error)
@@ -172,8 +173,8 @@ app.post("/findGame", express.json(), async (req,res)=>{
             }
         },
     });
-    //console.log(newGame);
-    return res.status(200).json({success:"match has started"});
+    return res.status(200).json({success:"match has started"})
+    //redirect to combat page
 });
 
 app.get("/getCurrentHp/:username", express.json(), async (req,res)=>{
